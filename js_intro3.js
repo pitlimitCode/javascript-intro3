6e8b66e602723136efd318bba01c30d512a6c188
pitlimitCode <63447990+pitlimitCode@users.noreply.github.com>
2022-06-14 09:06:04 +0700

    Update js_intro3.js

--- js_intro3.js
+++ js_intro3.js
@@ -122,41 +122,98 @@ switch (hariIni) {
 }
 
 {// 3. Buat 2 program bebas menggunakan promise seperti soal nomor 1
-// Program bebas 1
-const cekHariKerja2 = (day) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const dataDay = ['senin', 'selasa', 'rabu', 'kamis', 'jumat'];
-      let cek = dataDay.find((item) => {
-        return item === day;
-      })
-      if(cek){
-        resolve(cek);
-      } else {
-        reject(new Error('Hari ini bukan hari kerja'));
-      }
-    }, 3000)
-  })
-}
-// console.log(cekHariKerja2);
 
-// Program bebas 2
-const cekHariKerja3 = (day) => {
+{// Program bebas 1
+const timePres = (timer, youStop) => {
+
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const dataDay = ['senin', 'selasa', 'rabu', 'kamis', 'jumat'];
-      let cek = dataDay.find((item) => {
-        return item === day;
-      })
-      if(cek){
-        resolve(cek);
-      } else {
-        reject(new Error('Hari ini bukan hari kerja'));
+    
+    // waktu berhenti presentase
+    const ys = new Date().getHours() + ":" 
+              + new Date().getMinutes() + ":" 
+              + (new Date().getSeconds() + (youStop)); // youstop
+    
+    // batas waktu presentase
+    const tmr = new Date().getHours() + ":" 
+              + new Date().getMinutes() + ":" 
+              + (new Date().getSeconds() + (timer)); // timer
+
+    // setInterval, memproses dengan jarak/interval waktu
+    let setIntervalFunct = setInterval(() => {
+      // method untuk menghentikan setinterval
+      function timeStopFunc() {  clearInterval(setIntervalFunct); }
+      
+      // waktu yang berlangsung untuk ditampilkan
+      let count = new Date().getHours() + ":" 
+                + new Date().getMinutes() + ":" 
+                + new Date().getSeconds(); 
+      console.log(count);
+
+      // keterangan di console saat melebihi waktu presentase
+      if (count == tmr) {
+        console.log(`
+        MELEWATI BATAS WAKTU ...
+        `);
       }
-    }, 3000)
+      
+      // resolve dari Promise
+      if(count == ys && count < tmr){
+        resolve(youStop); // ke resolve ke .then
+        setTimeout(timeStopFunc, count);
+      }
+      
+      // resolve dari Promise
+      if (count == ys && count > tmr) {
+        reject((youStop-timer)); // ke reject ke .catch
+        setTimeout(timeStopFunc);
+      }
+
+    }, 1000);
   })
+};
+
+// // timePres = (timer, youStop);
+// // timePres(4,2)
+// timePres(2,4)
+//   .then((res)=>(console.log(`
+//   SELESAI.
+//   ANDA TELAH PRESENTASE SELAMA ${res} detik.
+// `)))
+//   .catch((err)=>(console.log(`
+//   SELESAI. TAPI MELEBIHI BATAS WAKTU 
+//   PRESENTASE SEBANYAK ${err} detik.
+// `)));
+
 }
-// console.log(cekHariKerja3);
+  
+{// Program bebas 2
+const diajakNgumpul = (tOf, balas) => {
+  return new Promise((resolve, reject = 100) => {
+
+    setTimeout(() => {
+
+      if(balas == null) {
+        console.log('(*Abaikan)');
+      } else if (tOf){
+        resolve(balas/1000);
+      } else {
+        reject(balas/1000);
+      }
+      
+    }, balas);
+    
+  })
+};
+
+// diajakNgumpul(true, 4000) // ( true/false , milisecond )
+//   .then((res) =>  ( console.log('Membalas pesan, ' + res + ' detik kemudian: Gaaas! (*siap-siap)')
+//   ))
+//   .catch((err) => ( console.log('Membalas pesan, ' + err + ' detik kemudian: Ngak. (*lanjut turu)')
+//   ))
+//   .finally( console.log('Membaca pesan: Hmmm... (mikir)'));
+
+}
+  
 }
 
 {// 4. Buatlah program menggunakan method fetch untuk menampilkan semua name (hanya name nya saja) dari REST API dibawah ini {https://jsonplaceholder.typicode.com/users}
